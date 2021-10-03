73f9eaa9a991b59a551f0033ba94970af7be6f01
maxleszcz <maxim.lish@gmail.com>
2021-10-03 19:51:18 +0300

    Added first layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -9,105 +11,227 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'DB App',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Color fontColor = Color.fromRGBO(255, 255, 255, 1.0);
+  Color buttonColor = Color.fromRGBO(255, 0, 0, 1.0);
+  Color textColor = Color.fromRGBO(0, 0, 0, 1.0);
+  Color greyColor = Color.fromRGBO(165, 165, 165, 1.0);
+
+  TextStyle smallTextStyle = TextStyle(
+    fontSize: 10,
+    color: Color.fromRGBO(0, 0, 0, 1.0),
+  );
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        title: Text(
+            "Plannung",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 15,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
         ),
+        backgroundColor: fontColor,
+        foregroundColor: fontColor,
+        leading: Icon(
+          Icons.menu,
+          color: textColor,
+        ),
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20),
+            child: GestureDetector(
+              onTap: () {},
+              child: Icon(
+                Icons.access_time_filled_sharp,
+                color: textColor,
+              ),
+            ),
+          ),
+
+        ],
+        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Expanded(child: Container(),),
+          Expanded(child: GestureDetector(onTap: (){},
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(child: Padding(
+                      padding: EdgeInsets.only(left: 60),
+                      child: Text(
+                        'Von',
+                        style: TextStyle(
+                          color: textColor,
+                        ),
+                      ),
+                    ), flex: 5,),
+                    Expanded(child: Padding(
+                      padding: EdgeInsets.only(right: 40),
+                      child: Icon(
+                        Icons.gps_fixed,
+                      ),
+                    ), flex: 1,)
+                  ],
+                ),
+                Padding(padding: EdgeInsets.only(left: 40, right: 40, bottom: 20),
+                  child: Divider(
+                    color: textColor,
+                  ),
+                )
+              ],
+            )
+          ),),
+          Expanded(child: GestureDetector(onTap: (){},
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(child: Padding(
+                        padding: EdgeInsets.only(left: 60),
+                        child: Text(
+                          'Nach',
+                          style: TextStyle(
+                            color: textColor,
+                          ),
+                        ),
+                      ), flex: 5,),
+                      Expanded(child: Padding(
+                        padding: EdgeInsets.only(right: 40),
+                        child: Icon(
+                          Icons.gps_fixed,
+                        ),
+                      ), flex: 1,)
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.only(left: 40, right: 40),
+                    child: Divider(
+                      color: textColor,
+                    ),
+                  )
+                ],
+              )
+          ),),
+          Expanded(child: Container(
+            height: 40,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Container(
+                color: greyColor,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(child: Padding(
+                      padding: EdgeInsets.only(left: 5),
+                      child: Icon(
+                        Icons.calendar_today,
+                        color: textColor,
+                      ),
+                    ), flex: 2,),
+                    Expanded(child: Text(
+                        "Wed, 3 Sep"
+                      ), flex: 4,),
+                    Expanded(child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 2, vertical: 8),
+                      child: VerticalDivider(
+                        color: textColor,
+                      ),
+                    ), flex: 1,),
+                    Expanded(child: Padding(
+                      padding: EdgeInsets.only(left: 5),
+                      child: Icon(
+                        Icons.access_time_outlined,
+                        color: textColor,
+                      ),
+                    ), flex: 2,),
+                    Expanded(child: Text(
+                      "Departure: 21:30"
+                    ), flex: 4,)
+                  ],
+                ),
+              ),
+            )
+          ),),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(child: Padding(padding: EdgeInsets.only(left: 40),child:Icon(
+                  Icons.airline_seat_recline_normal_rounded
+                )),),
+                Expanded(child: Column(
+
+                  children: <Widget>[
+                    Expanded(
+                    child: Padding(padding: EdgeInsets.only(top: 15),child: Text(
+                      "2nd Class | 1 Ad",
+                      style: smallTextStyle,
+                    ),
+                  ),),
+                    Expanded(
+                      child: Padding(padding: EdgeInsets.only(bottom: 5, top: 0),child: Text(
+                        "Show fastest connections",
+                        style: smallTextStyle,
+                      ),
+                      ),),
+
+                  ],
+                ), flex: 4),
+                Expanded(
+                  child: Container(),
+                  flex: 3,
+
+                ),
+              ],
+            ),
+          ),
+          Expanded(child: Container(
+
+            child: Container(
+              height: 30,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Container(
+                  color: buttonColor,
+                  child: GestureDetector(
+                    onTap: (){},
+                    child: Row(children: <Widget>[
+                      Expanded(child: Text(
+                      "Search",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: fontColor,
+                        fontSize: 20,
+                      ),
+                    )),]
+                  )),
+                ),
+              ),
+            ),
+          )),
+          Expanded(flex:5, child: Container())
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
   }
 }
